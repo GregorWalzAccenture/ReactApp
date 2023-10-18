85c6d8b3a5194a902204f02302527133d46d7c93
Christian Grobmeier <cg@grobmeier.de>
2023-10-18 15:54:21 +0200

    added stuff

---
+++ src/IntroText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class IntroText extends React.Component {
+    render() {
+        return (
+            <div>My text</div>
+        )
+    }
+}
+
+export default IntroText;
